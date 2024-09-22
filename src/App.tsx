f897542741538b0e2795bfc35b97dc3442cd1a56
Raalnan9 <Raalnan9@TraxxisDev.OnMicrosoft.com>
2024-09-21 23:27:06 -0400

    Refactor App component to update project name and add repository link

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
-import './App.css';
-import VideoDownload from './VideoDownload';
-import MediaDisplay from './MediaDisplay';
+import React from "react";
+import "./App.css";
+import VideoDownload from "./VideoDownload";
+import MediaDisplay from "./MediaDisplay";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Media Socializer</h1>
-        <p>Download videos to your <a target="downloads" href="http://localhost:5000/downloads">computer</a>.</p>
+        <p>
+          Download videos to your{" "}
+          <a target="downloads" href="http://localhost:5000/downloads">
+            computer
+          </a>
+          .
+        </p>
         <VideoDownload />
         <MediaDisplay />
         <div>
-          <a target="Repo" href = "https://github.com/Raalnan1/MediaSocializer">Repo</a>
+          <a target="Repo" href="https://github.com/Raalnan1/MediaSocializer">
+            Repo
+          </a>
         </div>
       </header>
     </div>
